a271a767c168e3fd01a16ccc778cb2f0711f7069
mirinai <tmddjs1771@gmail.com>
2024-07-24 23:14:46 +0900

    몰라

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+let pPhysicalDom = document.createElement('p');
+pPhysicalDom.innerText = 'Hello physical DOM world!';
+document.body.appendChild(pPhysicalDom);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const pVirtualDom = React.createElement('p', null, 'Hello world!');
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(pVirtualDom);
+
+let rootDiv = document.getElementById(`root`);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
